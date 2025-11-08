1e6e342bdaa5958eac96cd2d2384e574cfebacac
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2025-11-08 22:14:00 +0100

    solved syntax error of Logical OR (:

--- javascript/vehicle-purchase/vehicle-purchase.js
+++ javascript/vehicle-purchase/vehicle-purchase.js
@@ -11,7 +11,7 @@
  * @returns {boolean} whether a license is required
  */
 export function needsLicense(kind) {
-  if (kind == 'car' || 'truck') {
+  if (kind == 'car' || kind == 'truck') {
     return true
   } else {
     return false
@@ -45,9 +45,9 @@ export function chooseVehicle(option1, option2) {
 export function calculateResellPrice(originalPrice, age) {
   if (age < 3) {
     return originalPrice * 0.8
-  } else if (age > 10) {
-    return originalPrice * 0.5
-  } else {
+  } else if (age >= 3 && age <= 10) {
     return originalPrice * 0.7
+  } else {
+    return originalPrice * 0.5
   }
 }
