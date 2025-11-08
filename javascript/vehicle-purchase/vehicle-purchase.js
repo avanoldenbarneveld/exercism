866f5bc313e4726dc8287c2303bfed7e01a615b8
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2025-11-08 22:03:33 +0100

    initial solution for vehicle-purchase.js

--- javascript/vehicle-purchase/vehicle-purchase.js
+++ javascript/vehicle-purchase/vehicle-purchase.js
@@ -11,7 +11,13 @@
  * @returns {boolean} whether a license is required
  */
 export function needsLicense(kind) {
-  throw new Error('Remove this line and implement the function');
+  if (kind == 'car') {
+    return true
+  } else if (kind == 'truck') {
+    return true
+  } else {
+    return false
+  }
 }
 
 /**
@@ -23,7 +29,11 @@ export function needsLicense(kind) {
  * @returns {string} a sentence of advice which option to choose
  */
 export function chooseVehicle(option1, option2) {
-  throw new Error('Remove this line and implement the function');
+  if (option1 < option2) {
+    return `${option1} is clearly the better choice.`
+  } else {
+    return `${option2} is clearly the better choice.`
+  }
 }
 
 /**
@@ -35,5 +45,11 @@ export function chooseVehicle(option1, option2) {
  * @returns {number} expected resell price in the dealership
  */
 export function calculateResellPrice(originalPrice, age) {
-  throw new Error('Remove this line and implement the function');
+  if (age < 3) {
+    return originalPrice * 0.8
+  } else if (age > 10) {
+    return originalPrice * 0.5
+  } else {
+    return originalPrice * 0.7
+  }
 }
