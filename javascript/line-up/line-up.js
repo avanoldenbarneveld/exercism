5d7323908d6a2981ec475bf87b483243048b320e
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2026-03-25 09:11:03 +0100

    add line up exercise

---
+++ javascript/line-up/line-up.js
@@ -0,0 +1,8 @@
+//
+// This is only a SKELETON file for the 'Line Up' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const format = (name, number) => {
+  return `${name} you are the ${number} customer we serve today`
+};
